53a32201686d47fd20631435a34225efc27cec88
Cabreroknt <unaxgonzarodri@gmail.com>
2026-01-15 10:47:44 +0100

    Commit Persona

---
+++ Persona.java
@@ -0,0 +1,31 @@
+// Clase Persona
+public class Persona {
+    private String dni;
+    protected String nombre;
+    protected int edad;
+
+    // Constructor completo
+    public Persona(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // Método presentarse
+    public String presentarse() {
+        return "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+    }
+}
